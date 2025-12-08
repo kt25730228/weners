6bba0d3933f4dd1df6f1c74137a0251dc304c675
kt25730228 <kt25730228@gmail.com>
2025-12-08 15:55:21 +0900

    Delete build-config.js

--- build-config.js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs');
-
-const content = `window.SUPABASE_URL="${process.env.SUPABASE_URL}"; 
-window.SUPABASE_ANON_KEY="${process.env.SUPABASE_ANON_KEY}";`;
-
-fs.writeFileSync('./public/config.js',content);
